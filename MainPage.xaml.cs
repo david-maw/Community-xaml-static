aac41eeaf424c03276bf142bf36ab0d6cf40e1cd
David Maw <david@maw.name>
2024-12-09 09:13:18 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Community;
+
+
+public partial class MainPage : ContentPage
+{
+#if DEBUG
+    public static readonly bool IsDebug = true; // Not a const so as to avoid "unreachable code" warnings
+#else
+    public static readonly bool IsDebug = false;
+#endif
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        for (int i = 20; i > 0; i--)
+            Debug.WriteLine("*************************************************");
+    }
+}
---
+++ Services/Utilities.cs
@@ -0,0 +1,9 @@
+﻿namespace Community.Services;
+
+/// <summary>
+/// A general repository for handy capabilities used throughout the app
+/// </summary>
+public static class Utilities
+{
+    public static readonly bool FalseValue = false; // Not a const so as to avoid "unreachable code" warnings
+}
